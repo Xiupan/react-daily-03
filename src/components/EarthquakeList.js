eceb315d78cf5d90bd79838fecbb83d48880a265
Alan Hong <xiupan@gmail.com>
2017-09-20 16:13:08 -0500

    Changed displaying coordinates

--- src/components/EarthquakeList.js
+++ src/components/EarthquakeList.js
@@ -13,7 +13,7 @@ class EarthquakeList extends Component {
               <h4 className="card-title">{earthquake.properties.place}</h4>
               <h6 className="card-subtitle mb-2 text-muted">Magnitude: {earthquake.properties.mag}</h6>
               <h6 className="card-subtitle mb-2 text-muted">Time: {moment(earthquake.properties.time).format('llll')}</h6>
-              <p className="card-text">Coordinates: {earthquake.geometry.coordinates}</p>
+              <p className="card-text">Coordinates: X = {earthquake.geometry.coordinates[0]},  Y = {earthquake.geometry.coordinates[1]}, Z = {earthquake.geometry.coordinates[2]}</p>
 
               <a href={earthquake.properties.url} className="card-link">USGS Event Link</a>
 
